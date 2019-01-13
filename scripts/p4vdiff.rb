d46843b959dc2564749ddc604551b958a7306c30
zackel <legris.thomas@gmail.com>
2019-01-13 23:22:02 +0000

    Add diff tool

--- scripts/p4vdiff.rb
+++ scripts/p4vdiff.rb
@@ -1,30 +1,51 @@
+#!/usr/bin/ruby
 require 'tty-prompt'
 prompt = TTY::Prompt.new
 
-output=`ls`
+trap("SIGINT") { throw :ctrl_c }
 
-output_arr=[]
-output.each_line do |var|
-    sym = var.delete!("\n").to_sym
-    output_arr.push(sym)
-end
+tuple = Struct.new(:_1, :_2)
+@tmpdir = "/tmp/p4v"
+`mkdir -p #{@tmpdir}/#{changeListNum}`
 
-diffAll = prompt.yes?("Diff all?")
+catch :ctrl_c do
+    begin
+        print "Diff changelist#: "
+        changeListNum = gets.chomp;
+        raise 'Empty string passed' if changeListNum.empty?
+        raise 'Change # not a number' if changeListNum =~ /\D/
 
-chosen_paths = []
-if diffAll
-    output_arr.each do |choice|
-        chosen_paths.append choice
-    end
-else
-    choices = prompt.multi_select("Choose between:") do |menu|
-        output_arr.each do |var|
-            menu.choice var
+        output=`p4 describe #{changeListNum}`
+        puts "#{output}"
+
+        filenames=output.scan(/\.\.\. (.*#[0-9]+) .*/).flatten
+        downloaded_filenames = Hash.new
+        filenames.each do |filename|
+            escape_filename=filename.gsub(/\//,
+                                          '')
+            escape_filename=escape_filename.gsub(/(\..*)#([0-9]+)/,
+                                                 '__\2\1')
+            `p4 print -q #{filename} > "#{@tmpdir}/#{changeListNum}/.#{escape_filename}"`
+            crev_filename=filename.gsub(/#[0-9]+/,
+                                        "@=#{changeListNum}")
+            `p4 print -q #{crev_filename} >  #{@tmpdir}/#{changeListNum}/#{escape_filename}`
+            downloaded_filenames[filename] = tuple.new("#{@tmpdir}/#{changeListNum}/#{escape_filename}",
+                                                       "#{@tmpdir}/#{changeListNum}/.#{escape_filename}")
         end
-    end
-    choices.each do |choice|
-        chosen_paths.append choice.to_sym
-    end
-end
 
-puts "#{chosen_paths}"
+        diffAll = prompt.yes?("Diff all? (#{downloaded_filenames.length} files)")
+
+        chosen_paths = []
+        if diffAll
+            downloaded_filenames.each do |key, value|
+                chosen_paths.append value
+            end
+        else
+            choices = prompt.multi_select("Choose between:") do |menu|
+                downloaded_filenames.each do |key, value|
+                    menu.choice key
+                end
+            end
+            choices.each do |choice|
+                chosen_paths.append downloaded_filenames[choice]
+            end
